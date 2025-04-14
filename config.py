e7c410bbaa77fd7ef72c9022099bc0ca6d8baba6
Alessia Ciaccia <136704910+AleeCiacciaa@users.noreply.github.com>
2025-04-14 13:01:40 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+file_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/124103_cont_7MTM2_jointdeconv.image.pbcor.fits'
+new_file_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/new_file.fits'
+verification_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/verification.fits'
+file_path = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/cat_7MTM2_ipac.txt'
+file_regionsds9 = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/file_regionsds9.reg'
+cube_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/124103_spw1_7MTM2_jointdeconv.image.fits'
+pb_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/124103_cont_7MTM2_jointdeconv.pb.fits'
+cube_pb_fits = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/124103_spw1_7MTM2_jointdeconv.image.pbcor.fits'
+plot_file = '/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/plotting/plot_image.jpg'
+data_freqs_file ='/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/plotting/data_freqs.txt'
+data_velocities_file ='/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/plotting/data_velocities.txt'
+data_freqs_pixel_file ='/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/plotting/data_freqs_pixel.txt'
+data_velocities_pixel_file ='/mnt/c/Users/02ale/Desktop/tirocinio-astrofisica/combined-cont-fits/almagal/cube/plotting/data_velocities_pixel.txt'
+
+value_clump = '124103'
+# value_clump = '100309' #for tryng another clump with 5 kers
+column_name = 'RA' #change this name according to the column whose type and units you want to know about
+ker_plot = 4 
