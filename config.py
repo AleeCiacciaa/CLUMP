bef8a295de0d69b30ee04e1c5d5558ed9c7f0a87
Alessia Ciaccia <136704910+AleeCiacciaa@users.noreply.github.com>
2025-04-17 17:32:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,32 +5,47 @@ verification_fits = 'combined-cont-fits/verification.fits'
 file_path = 'cat_7MTM2_ipac.txt'
 file_regionsds9 = 'combined-cont-fits/file_regionsds9.reg'
 
-cube_fits_spw0 = 'combined-cont-fits/almagal/cube/124103_spw0_7MTM2_jointdeconv.image.fits'
-cube_fits_spw1 = 'combined-cont-fits/almagal/cube/124103_spw1_7MTM2_jointdeconv.image.fits'
-cube_fits = cube_fits_spw0 #default
+#spw0 default
+cube_fits_spw0 = 'combined-line-fits/almagal/124103_spw0_7MTM2_jointdeconv.image.fits'
+cube_fits_spw1 = 'combined-line-fits/almagal/124103_spw1_7MTM2_jointdeconv.image.fits'
+cube_fits = cube_fits_spw0 
 
 pb_fits = 'combined-cont-fits/almagal/124103_cont_7MTM2_jointdeconv.pb.fits'
-cube_pb_fits = 'combined-cont-fits/almagal/cube/124103_spw1_7MTM2_jointdeconv.image.pbcor.fits'
-plot_file = 'combined-cont-fits/almagal/cube/plotting/plot_image.jpg'
-data_freqs_file ='combined-cont-fits/almagal/cube/plotting/data_freqs.txt'
-data_velocities_file ='combined-cont-fits/almagal/cube/plotting/data_velocities.txt'
-data_freqs_pixel_file ='combined-cont-fits/almagal/cube/plotting/data_freqs_pixel.txt'
-data_velocities_pixel_file ='combined-cont-fits/almagal/cube/plotting/data_velocities_pixel.txt'
+cube_pb_fits_spw0 = '/combined-line-fits/almagal/124103_spw0_7MTM2_jointdeconv.image.pbcor.fits'
+cube_pb_fits_spw1 = 'combined-line-fits/almagal/124103_spw1_7MTM2_jointdeconv.image.pbcor.fits'
+cube_pb_fits = cube_pb_fits_spw0
 
-sliced_cube_H2CO_fits ='combined-cont-fits/almagal/cube/momentumn/sliced_H2CO_cube.fits'
-moment0_H2CO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom0_H2CO.fits'
-moment1_H2CO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom1_H2CO.fits'
-moment2_H2CO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom2_H2CO.fits'
+plot_file_spw0 = 'combined-line-fits/plotting/spw0/plot_image_spw0.jpg'
+data_freqs_file_spw0 ='combined-line-fits/plotting/spw0/data_freqs_spw0.txt'
+data_velocities_file_spw0 ='combined-line-fits/plotting/spw0/data_velocities_spw0.txt'
+data_freqs_pixel_file_spw0 ='combined-line-fits/plotting/spw0/data_freqs_pixel_spw0.txt'
+data_velocities_pixel_file_spw0 ='combined-line-fits/plotting/spw0/data_velocities_pixel_spw0.txt'
 
-sliced_cube_SiO_fits ='combined-cont-fits/almagal/cube/momentumn/sliced_SiO_cube.fits'
-moment0_SiO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom0_SiO.fits'
-moment1_SiO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom1_SiO.fits'
-moment2_SiO_fits = 'combined-cont-fits/almagal/cube/momentumn/mom2_SiO.fits'
+plot_file_spw1 = 'combined-line-fits/plotting/spw1/plot_image_spw1.jpg'
+data_freqs_file_spw1 ='combined-line-fits/plotting/spw1/data_freqs_spw1.txt'
+data_velocities_file_spw1 ='combined-line-fits/plotting/spw1/data_velocities_spw1.txt'
+data_freqs_pixel_file_spw1 ='combined-line-fits/plotting/spw1/data_freqs_pixel_spw1.txt'
+data_velocities_pixel_file_spw1 ='combined-line-fits/plotting/spw1/data_velocities_pixel_spw1.txt'
 
-sliced_cube_CH3CN_fits ='combined-cont-fits/almagal/cube/momentumn/sliced_CH3CN_cube.fits'
-moment0_CH3CN_fits = 'combined-cont-fits/almagal/cube/momentumn/mom0_CH3CN.fits'
-moment1_CH3CN_fits = 'combined-cont-fits/almagal/cube/momentumn/mom1_CH3CN.fits'
-moment2_CH3CN_fits = 'combined-cont-fits/almagal/cube/momentumn/mom2_CH3CN.fits'
+plot_file = plot_file_spw0
+data_freqs_file = data_freqs_file_spw0
+data_velocities_file = data_velocities_file_spw0
+data_freqs_pixel_file = data_freqs_pixel_file_spw0
+
+sliced_cube_H2CO_fits ='combined-line-fits/momentumn/sliced_H2CO_cube.fits'
+moment0_H2CO_fits = 'combined-line-fits/momentumn/mom0_H2CO.fits'
+moment1_H2CO_fits = 'combined-line-fits/momentumn/mom1_H2CO.fits'
+moment2_H2CO_fits = 'combined-line-fits/momentumn/mom2_H2CO.fits'
+
+sliced_cube_SiO_fits ='combined-line-fits/momentumn/sliced_SiO_cube.fits'
+moment0_SiO_fits = 'combined-line-fits/momentumn/mom0_SiO.fits'
+moment1_SiO_fits = 'combined-line-fits/momentumn/mom1_SiO.fits'
+moment2_SiO_fits = 'combined-line-fits/momentumn/mom2_SiO.fits'
+
+sliced_cube_CH3CN_fits ='combined-line-fits/momentumn/sliced_CH3CN_cube.fits'
+moment0_CH3CN_fits = 'combined-line-fits/momentumn/mom0_CH3CN.fits'
+moment1_CH3CN_fits = 'combined-line-fits/momentumn/mom1_CH3CN.fits'
+moment2_CH3CN_fits = 'combined-line-fits/momentumn/mom2_CH3CN.fits'
 
 #H2CO default
 sliced_cube_fits = sliced_cube_H2CO_fits
