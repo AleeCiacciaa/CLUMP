df5466f4dbfbcb406c50ec25e9b9a777d1b0ee1c
Alessia Ciaccia <136704910+AleeCiacciaa@users.noreply.github.com>
2025-04-17 17:26:45 +0200

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -13,7 +13,8 @@ from def_astro import (
     compare_integral_flux,
     find_background,
     compare_integral_flux_background,
-    plot_spectrum
+    plot_spectrum,
+    momentumn
 )
 
 from cube_pb_fits import divide_cube_by_pb
@@ -39,25 +40,15 @@ def main():
     decision = analisys()
 
     if decision == "1":
-        print(f"\nConitnuous analysis running...")
-
-        ans = input(f"\nDefault CLUMP value is {config.value_clump}, do you want to keep it?[y/n] ")
-        if ans.lower() == "n":
-            value_clump = input("Enter new CLUMP value: ")
-            print(f"New CLUMP value set to {value_clump}")
-        else:
-            value_clump = config.value_clump
-            print(f"Using defaul CLUMP value: {config.value_clump}")
-
-        hdul, data, header = info_fits(config.file_fits) #from function info_fits
+        print(f"\nContinuous analysis of CLUMP {config.value_clump} running.")
+        hdul, data, header = info_fits(config.file_fits) 
         F, arcsec_per_pixel = conversion_factor(header)
         converted_data, updated_header = conversion_data(hdul, data, header, F)
         creating_new_file_fits(converted_data, updated_header, config.new_file_fits)
         verify_conversion(config.file_fits, config.new_file_fits, config.verification_fits, updated_header)
         hdul.close()
-        ra_values, dec_values, FWHM_circ_values, region_radius_values, F_INT_values = reading_table(config.file_path, value_clump)
+        ra_values, dec_values, FWHM_circ_values, region_radius_values, F_INT_values = reading_table(config.file_path, config.value_clump)
         pixel_coordinates, x_values, y_values  = conversion_RA_DEG_pixel(ra_values, dec_values, header)
-        coordinates_table(pixel_coordinates, region_radius_values)
         fluxes, discrepancies, pixels_count, region_radius_values_pixel = integral_flux(x_values, y_values, region_radius_values, arcsec_per_pixel, data, F_INT_values)
         file_create_region(x_values, y_values, region_radius_values_pixel, config.file_regionsds9)
         compare_integral_flux(F_INT_values, fluxes, discrepancies)
@@ -65,41 +56,73 @@ def main():
         compare_integral_flux_background(F_INT_values, fluxes_background, discrepancies_backgoround)
         
     elif decision == "2":
-        print("\nCube analysis running...")
-
-        ans1 = input(f"\nDefault CLUMP value is {config.value_clump}, do you want to keep it?[y/n] ")
+        print(f"\nCube analysis of CLUMP {config.value_clump} running.")
+        ans1 = input(f"Default KER plot is {config.ker_plot}, do you want to keep it?[y/n] ")
         if ans1.lower() == "n":
-            value_clump = input("Enter new CLUMP value: ")
-        else:
-            value_clump = config.value_clump
-            print(f"Using defaul CLUMP value: {config.value_clump}")
-
-        ans2 = input(f"\nDefault KER plot is {config.ker_plot}, do you want to keep it?[y/n] ")
-        if ans2.lower() == "n":
             ker_plot = input("Enter new KER plot: ")
         else:
             ker_plot = config.ker_plot
             print(f"Using defaul KER plot: {config.ker_plot}")
         
+        print("\nSelect spectral window:")
+        print("[0] spw0")
+        print("[1] spw1")
+        ans2 = input("Select: ")
+        if ans2 == "0":
+            cube_fits = config.cube_fits_spw0
+            plot_file = config.plot_file_spw0
+            data_freqs_file = config.data_freqs_file_spw0
+            data_velocities_file = config.data_velocities_file_spw0
+            data_freqs_pixel_file = config.data_freqs_pixel_file_spw0
+            data_velocities_pixel_file = config.data_velocities_pixel_file_spw0
+
+        else:
+            cube_fits = config.cube_fits_spw1
+            plot_file = config.plot_file_spw1
+            data_freqs_file = config.data_freqs_file_spw1
+            data_velocities_file = config.data_velocities_file_spw1
+            data_freqs_pixel_file = config.data_freqs_pixel_file_spw1
+            data_velocities_pixel_file = config.data_velocities_pixel_file_spw1
+
         with open(os.devnull, 'w') as fnull:
             with redirect_stdout(fnull):
                 hdul, data, header = info_fits(config.file_fits)
                 F, arcsec_per_pixel = conversion_factor(header)
-                ra_values, dec_values, FWHM_circ_values, region_radius_values, F_INT_values = reading_table(config.file_path, value_clump)
+                ra_values, dec_values, FWHM_circ_values, region_radius_values, F_INT_values = reading_table(config.file_path, config.value_clump)
                 pixel_coordinates, x_values, y_values  = conversion_RA_DEG_pixel(ra_values, dec_values, header)
                 fluxes, discrepancies, pixels_count, region_radius_values_pixel = integral_flux(x_values, y_values, region_radius_values, arcsec_per_pixel, data, F_INT_values)
         
-            plot_spectrum(ker_plot, config.c, config.cube_fits, x_values, y_values, region_radius_values_pixel, config.plot_file, config.data_freqs_file, config.data_velocities_file, config.data_freqs_pixel_file, config.data_velocities_pixel_file, position=None)
+            plot_spectrum(ker_plot, config.c, cube_fits, x_values, y_values, region_radius_values_pixel, plot_file, data_freqs_file, data_velocities_file, data_freqs_pixel_file, data_velocities_pixel_file, position=None)
 
     elif decision == "3":
-        print("\nRunning main_script.py using default CLUMP value: {config.value_clump} and default KER plot:{config.ker_plot}.")
+        print(f"\nRunning main_script.py using default CLUMP value {config.value_clump}, default KER plot {config.ker_plot} and default spectral wind 0.")
         subprocess.run(["python3", "main_script.py"])
 
     elif decision == "4":
-        divide_cube_by_pb(config.cube_fits, config.pb_fits, config.cube_pb_fits)
+        print("\nSelect spectral window:")
+        print("[0] spw0")
+        print("[1] spw1")
+        ans = input("Select: ")
 
-    elif decision == "5":      
+        if ans == "0":
+            cube_fits = config.cube_fits_spw0
+            cube_pb_fits = config.cube_pb_fits_spw0
+        else:
+            cube_fits = config.cube_fits_spw1
+            cube_pb_fits = config.cube_pb_fits_spw1
+
+        divide_cube_by_pb(cube_fits, config.pb_fits, cube_pb_fits)
+
+    elif decision == "5":
+        ans = input(f"\nDefault CLUMP value is {config.value_clump}, do you want to keep it?[y/n] ")
+        if ans.lower() == "n":
+            value_clump = input("Enter new CLUMP value: ")
+        else:
+            value_clump = config.value_clump
+            print(f"Using defaul CLUMP value: {config.value_clump}")
+
         column_name = input("\nName of the column you want to analysis: ")
+        ra_values, dec_values, FWHM_circ_values, region_radius_values, F_INT_values = reading_table(config.file_path, value_clump)
         units_type(config.file_path, value_clump, column_name)
 
     elif decision == "6":
@@ -111,9 +134,9 @@ def main():
 
         if dec0 == "1":
             mol = "H2CO"
-            nu_rest = 218.222192     #GHz
-            freq_min = 218.15        #GHz
-            freq_max = 218.16        #GHz
+            nu_rest = 218.222192    #GHz
+            freq_min = 218.15       #GHz
+            freq_max = 218.16       #GHz
             cube_fits = config.cube_fits_spw0
             moment0_fits = config.moment0_H2CO_fits
             moment1_fits = config.moment1_H2CO_fits
@@ -122,9 +145,9 @@ def main():
 
         elif dec0 == "2":
             mol = "SiO"
-            nu_rest = 217.10498      #GHz
-            freq_min = 217.02        #GHz
-            freq_max = 217.05        #GHz
+            nu_rest = 217.10498     #GHz
+            freq_min = 217.02       #GHz
+            freq_max = 217.05       #GHz
             cube_fits = config.cube_fits_spw0
             moment0_fits = config.moment0_SiO_fits
             moment1_fits = config.moment1_SiO_fits
@@ -133,9 +156,9 @@ def main():
 
         elif dec0 == "3":
             mol = "CH3CN"
-            nu_rest = 220.747261     #GHz
-            freq_min = 220.67        #GHz
-            freq_max = 220.68        #GHz
+            nu_rest = 220.747261    #GHz
+            freq_min = 220.67       #GHz
+            freq_max = 220.68       #GHz
             cube_fits = config.cube_fits_spw1
             moment0_fits = config.moment0_CH3CN_fits
             moment1_fits = config.moment1_CH3CN_fits
@@ -149,14 +172,12 @@ def main():
         print(f"Nu rest for {mol} is set to {nu_rest} GHz.")
         print("It may take few seconds.")
         momentumn(cube_fits, freq_min, freq_max, config.c, nu_rest, moment0_fits, moment1_fits, moment2_fits, sliced_cube_fits)
- 
-    
+        
     elif decision == "0":
         print("Exit from the programme")
 
     else: 
         print("Not valid answrer, try again.")
-
 
 if __name__ == "__main__":
     main()
